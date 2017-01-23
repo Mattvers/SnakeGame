9d9afbcf127ef37f87bea5663f93d0ad65075536
Mattvers <mateusz.grzegorzewicz@gmail.com>
2017-01-23 14:41:52 +0100

    First implementation. Adding Graphic Game window

---
+++ SnakeGame/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace SnakeGame
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBoxCanvas = new System.Windows.Forms.PictureBox();
+            this.labelName = new System.Windows.Forms.Label();
+            this.labelScore = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxCanvas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxCanvas
+            // 
+            this.pictureBoxCanvas.Location = new System.Drawing.Point(12, 12);
+            this.pictureBoxCanvas.Name = "pictureBoxCanvas";
+            this.pictureBoxCanvas.Size = new System.Drawing.Size(663, 415);
+            this.pictureBoxCanvas.TabIndex = 0;
+            this.pictureBoxCanvas.TabStop = false;
+            // 
+            // labelName
+            // 
+            this.labelName.AutoSize = true;
+            this.labelName.Font = new System.Drawing.Font("Algerian", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelName.Location = new System.Drawing.Point(270, 433);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(111, 31);
+            this.labelName.TabIndex = 1;
+            this.labelName.Text = "SCORE:";
+            // 
+            // labelScore
+            // 
+            this.labelScore.AutoSize = true;
+            this.labelScore.Font = new System.Drawing.Font("Algerian", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelScore.Location = new System.Drawing.Point(387, 433);
+            this.labelScore.Name = "labelScore";
+            this.labelScore.Size = new System.Drawing.Size(31, 31);
+            this.labelScore.TabIndex = 2;
+            this.labelScore.Text = "0";
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(682, 473);
+            this.Controls.Add(this.labelScore);
+            this.Controls.Add(this.labelName);
+            this.Controls.Add(this.pictureBoxCanvas);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FormMain";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SnakeGame";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxCanvas)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxCanvas;
+        private System.Windows.Forms.Label labelName;
+        private System.Windows.Forms.Label labelScore;
+    }
+}
+
