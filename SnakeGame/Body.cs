8978a75104e861b05c6fce78eb16f6d379daa9b8
Mattvers <mateusz.grzegorzewicz@gmail.com>
2017-01-23 14:56:12 +0100

    Adding SnakeBody class and InputsButtons class

---
+++ SnakeGame/Body.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeGame
+{
+    //class body to remember positions of snake's body.
+    class Body
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        //constructor of class, default values to 0
+        public Body()
+        {
+            X = 0;
+            Y = 0;
+        }
+    }
+}
